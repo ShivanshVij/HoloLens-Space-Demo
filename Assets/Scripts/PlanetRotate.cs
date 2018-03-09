46da19243af3baf9b5f00050ed72f62c78235d71
Unknown <s6vij@edu.uwaterloo.ca>
2018-03-09 03:51:38 -0500

    Fixed Git Ignore

---
+++ Assets/Scripts/PlanetRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour {
+    public int speedx = 50;
+    public int speedy = 50;
+    public int speedz = 50;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(Time.deltaTime * speedx, Time.deltaTime * speedy, Time.deltaTime * speedz));	
+	}
+}
