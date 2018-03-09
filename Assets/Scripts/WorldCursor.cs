20e5584f9e9d5f0ad15b30b6e3ccae782db1380e
Unknown <s6vij@edu.uwaterloo.ca>
2018-03-09 03:51:38 -0500

    Added basic staging
    Implemented correct lighting, planet rotation scripts, as well as a heavily modified world cursor

---
+++ Assets/Scripts/WorldCursor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCursor : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    public Camera mainCamera;
+    private int COUNT = 0;
+    public int TIME = 300;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = mainCamera.transform.position;
+        var gazeDirection = mainCamera.transform.forward;
+
+        RaycastHit hitInfo;
+        Debug.DrawRay(transform.position, gazeDirection, Color.green);
+        if(Physics.Raycast(headPosition, gazeDirection, out hitInfo)) //While raycast is hitting an object
+        {
+            COUNT++;
+            //Debug.Log(COUNT);
+            if (COUNT >= TIME) {
+                Debug.Log("HIT!");
+                COUNT = 1;
+            }
+        }
+    }
+}
